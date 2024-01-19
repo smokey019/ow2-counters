330fb5652ac7d2aef1ebf268da072df3a72ff762
Mikkel <mikkelkulk@gmail.com>
2024-01-19 09:25:04 +0100

    Fixing errors

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ type Props = {}
 function Footer({}: Props) {
     return (
         <div className='w-1/2 mx-auto mt-12 text-sm text-center opacity-40'>
-            The Overwatch heroes' portraits are the property of Blizzard Entertainment.
+            The Overwatch heroes&apos; portraits are the property of Blizzard Entertainment.
         </div>
     )
 }
