87b1ead86660c37453b3db375f478f6c06777792
Mikkel <mikkelkulk@gmail.com>
2024-01-19 22:45:31 +0100

    Ability to use search params. color scheme

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,22 @@ import Image from 'next/image'
 import MainLayout from "@/components/MainLayout";
 import { heroes } from '@/data/heroes'
 import { HeroCountersProps, HeroProps } from "@/lib/types";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useSearchParams } from 'next/navigation';
 
 export default function Home() {
+  const searchParams = useSearchParams();
+  const hero_param = searchParams.get('hero')
   const [heroData, setHeroData] = useState<HeroProps | null>()
   const [selectedRole, setSelectedRole] = useState<string>("all"); // State to store selected role
   const [filteredHeroes, setFilteredHeroes] = useState<HeroProps[]>(heroes); // State to store filtered heroes based on role
+
+  useEffect(() => {
+    const found_hero = heroes.find(hero => hero.key === hero_param)
+    if(!found_hero) return
+    setHeroData(found_hero)
+    setSelectedRole(found_hero.role)
+  }, [hero_param])
 
   const handleRoleSelect = (role: string) => {
     setHeroData(null)
@@ -69,7 +79,7 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className='bg-accent/10 py-4 border-b border-t border-accent/35'>
+      <div className='py-4 border-b border-t border-accent/35'>
         <h1 className='text-center text-4xl font-semibold'>
           {heroData ? heroData.name : "Pick a hero!"}
         </h1>
